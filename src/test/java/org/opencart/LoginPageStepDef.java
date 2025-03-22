c44b801ab689f4ac47739d41a4c6e13b832aa0c9
Kalai <kalairam82@gmail.com>
2025-03-21 19:53:25 -0500

    Changes

--- src/test/java/org/opencart/LoginPageStepDef.java
+++ src/test/java/org/opencart/LoginPageStepDef.java
@@ -1,4 +1,4 @@
-package features;
+package org.opencart;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.After;
@@ -10,7 +10,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.qa.opencart.pages.Loginpage;
 import org.testng.Assert;
 public class LoginPageStepDef {
-
     private WebDriver driver;
     private Loginpage loginPage;
 
@@ -28,21 +27,35 @@ public class LoginPageStepDef {
 
     @Given("I am on the OpenCart login page")
     public void i_am_on_the_open_cart_login_page() {
-        driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login ");
-        loginPage = new Loginpage(driver);
+        {
+            driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login ");
+            loginPage = new Loginpage(driver);
+        }
     }
+
     @Given("I have entered a valid username and password")
-    public void i_have_entered_a_valid_username_and_password() {
-        loginPage.enterEmail("septbatch2024@open.com");
-        loginPage.enterPassword("Selenium@12345");
+    public void i_have_entered_a_valid_username_and_password(){
+        {
+            loginPage.enterEmail("septbatch2024@open.com");
+            loginPage.enterPassword("Selenium@12345");
+
+        }
 
     }
+
     @When("I click on the login button")
-    public void i_click_on_the_login_button() {
-        loginPage.clickLoginButton();
+    public void ii_click_on_the_login_button() {
+        {
+            loginPage.clickLoginButton();
+        }
+
     }
+
     @Then("I should be logged in successfully")
-    public void i_should_be_logged_in_successfully() {
+    public void i_should_be_logged_in_successfully(){
         Assert.assertEquals(loginPage.getLoginPageTitle(),"My Account");
     }
+
+
 }
+
--- src/test/resources/features/loginpage.feature
+++ src/test/resources/features/loginpage.feature
@@ -8,7 +8,7 @@ Feature: LoginPage functionality of E-commerce Application
   Background:
     Given I am on the OpenCart login page
 
-  Scenario: Successful login with valid credentials
+  Scenario:Successful login with valid credentials
     Given I have entered a valid username and password
     When I click on the login button
     Then I should be logged in successfully
